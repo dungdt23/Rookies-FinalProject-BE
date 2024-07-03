81c4b947eafeb04215fc9285e824c725859bede3
JaclyNolan <anhbg330011@gmail.com>
2024-07-03 11:44:37 +0700

    Done if user didn't change password for the first time they won't have access to the api

--- AssetManagement.Application/Exceptions/Assignment/AssignmentNotAcceptedException.cs
+++ AssetManagement.Application/Exceptions/Assignment/AssignmentNotAcceptedException.cs
@@ -2,6 +2,8 @@
 {
     public class AssignmentNotAcceptedException : Exception
     {
+        public AssignmentNotAcceptedException() : base() { }
         public AssignmentNotAcceptedException(string message) : base(message) { }
+        public AssignmentNotAcceptedException(string? message, Exception? innerException) : base(message, innerException) { }
     }
 }
--- AssetManagement.Application/Exceptions/Common/NotFoundException.cs
+++ AssetManagement.Application/Exceptions/Common/NotFoundException.cs
@@ -2,6 +2,8 @@
 {
     public class NotFoundException : Exception
     {
+        public NotFoundException() : base() { }
         public NotFoundException(string message) : base(message) { }
+        public NotFoundException(string? message, Exception? innerException) : base(message, innerException) { }
     }
 }
