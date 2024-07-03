186cf696795712632362425404202e1b5d49d228
JaclyNolan <anhbg330011@gmail.com>
2024-07-03 17:48:04 +0700

    Fix unit tests for JwtInvalidationService

--- AssetManagement.Application/Exceptions/Token/PasswordNotChangedException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AssetManagement.Application.Exceptions.Token
-{
-    public class PasswordNotChangedException : Exception
-    {
-        public PasswordNotChangedException() : base() { }
-        public PasswordNotChangedException(string message) : base(message) { }
-        public PasswordNotChangedException(string? message, Exception? innerException) : base(message, innerException) { }
-    }
-}
---
+++ AssetManagement.Application/Exceptions/Token/PasswordNotChangedFirstTimeException.cs
@@ -0,0 +1,9 @@
+﻿namespace AssetManagement.Application.Exceptions.Token
+{
+    public class PasswordNotChangedFirstTimeException : Exception
+    {
+        public PasswordNotChangedFirstTimeException() : base() { }
+        public PasswordNotChangedFirstTimeException(string message) : base(message) { }
+        public PasswordNotChangedFirstTimeException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
