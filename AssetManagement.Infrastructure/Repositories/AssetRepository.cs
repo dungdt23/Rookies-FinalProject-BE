89557860de6ef033fc95f5ce9f35fc18479890f3
drfred <dungtiendao01@gmail.com>
2024-07-04 15:05:36 +0700

    Edit query due to slow response

--- AssetManagement.Infrastructure/Repositories/AssetRepository.cs
+++ AssetManagement.Infrastructure/Repositories/AssetRepository.cs
@@ -19,9 +19,9 @@ namespace AssetManagement.Infrastructure.Repositories
             IQueryable<Asset> query = _context.Assets
                                     .Include(x => x.Location)
                                     .Include(x => x.Category)
-                                    .Include(x => x.Assignments.OrderBy(a => a.CreatedAt).Take(3))
+                                    .Include(x => x.Assignments.Take(3))
                                     .ThenInclude(x => x.Assignee)
-                                    .Include(x => x.Assignments.OrderBy(a => a.CreatedAt).Take(3))
+                                    .Include(x => x.Assignments.Take(3))
                                     .ThenInclude(x => x.Assigner);
             if (string.IsNullOrEmpty(filter.search) && !filter.state.HasValue && !filter.category.HasValue)
             {
