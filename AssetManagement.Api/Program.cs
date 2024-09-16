6c46ed51a19c6cdb39b16f7a8306da7c8420351a
dungdt23 <dungtiendao01@gmail.com>
2024-09-16 11:04:41 +0700

    Modify port

--- AssetManagement.Api/Program.cs
+++ AssetManagement.Api/Program.cs
@@ -145,6 +145,10 @@ namespace AssetManagement.Api
 
 			var app = builder.Build();
 
+			// Get the port from the environment, default to 80 if not set
+			var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+			app.Urls.Add($"http://*:{port}");
+
 			if (!app.Environment.IsDevelopment())
 			{
 				app.UseMiddleware<ApiExceptionHandlingMiddleware>();
